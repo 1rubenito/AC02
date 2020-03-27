0392e9ab08b235bff5d2e92af749d74f7aec07a5
Rubens Gianfaldoni <52938520+1rubenito@users.noreply.github.com>
2020-03-27 20:59:12 -0300

    update

--- n_primos.py
+++ n_primos.py
@@ -1,9 +1,3 @@
-import os
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
-
-@app.route('/')
 primos = [2]
 print(2, end=' ')
 i = 3
@@ -17,7 +11,4 @@ while len(primos) < 100:
     i += 1
 print('<---- 100˚ Número Primo!')
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
 
